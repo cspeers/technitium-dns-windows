fd94e4744e695c7fdf964a6497c385c5127de6b8
unknown <administrator@SECRETWEAPON.CO>
2020-10-22 18:18:01 -0500

    version

--- build.config.ps1
+++ build.config.ps1
@@ -1,5 +1,5 @@
 $env:DOCKER_REPO = "cspeers"
 $env:DOCKER_IMAGE = "technitium-dns"
-$env:DOCKER_APPLICATION_VERSION = "5.3"
+$env:DOCKER_APPLICATION_VERSION = "5.4"
 $env:DOCKER_IMAGE_TAG='3.1'
 $env:DOCKER_BASE_IMAGE = "mcr.microsoft.com/dotnet/core/sdk:$env:DOCKER_IMAGE_TAG"
