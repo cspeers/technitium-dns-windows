cd3f2a3500b0119a1b8b59c3b56780050d13c6c7
Chris Speers <chris.speers@outlook.com>
2023-03-09 17:49:35 -0600

    version

--- build.config.ps1
+++ build.config.ps1
@@ -1,5 +1,5 @@
 $env:DOCKER_REPO = "cspeers"
 $env:DOCKER_IMAGE = "technitium-dns"
-$env:DOCKER_APPLICATION_VERSION = "10.0"
+$env:DOCKER_APPLICATION_VERSION = "10.0.1"
 $env:DOCKER_IMAGE_TAG='7.0'
 $env:DOCKER_BASE_IMAGE = "mcr.microsoft.com/dotnet/runtime:$env:DOCKER_IMAGE_TAG"
